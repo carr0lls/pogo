5869c52a43e9c840d35c4808ed909dc74ad68649
Carroll Yu <cyu@commonsense.org>
2016-07-25 17:55:08 -0700

    add config file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
-var exec = require('child_process').exec;
+var execFile = require('child_process').execFile;
+var fs = require('fs');
 
 var app = express();
 var status;
+var CONFIG_FILE = path.join(__dirname, 'config.json');
 
-app.set('port', 3000);
+app.set('port', 4000);
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,26 +17,33 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
-app.get('/start', function(req, res){
-  if (status) {
-  	res.send('server is already running!');
+app.get('/start', function(req, res) {
+  	if (status) {
+  		res.send('server is already running!');
 	}
 	else {
-		status = exec('./start',
-			function (error, stdout, stderr) {
-				console.log('stdout: ' + stdout);
-				console.log('stderr: ' + stderr);
-			if (error !== null) {
-				console.log('exec error: ' + error);
+		fs.readFile(CONFIG_FILE, function(err, data) {
+	    	var CONFIG = data ? JSON.parse(data) : [];
+	    	var options = '';
+			for (setting in CONFIG) {
+				options += CONFIG[setting].key + ' ' + CONFIG[setting].value + ' ';
 			}
-		});	
-		res.send('starting server...');
+			status = execFile('./start', [options], function (error, stdout, stderr) {
+					console.log('stdout: ' + stdout);
+					console.log('stderr: ' + stderr);
+
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			});	
+			res.send('starting server...');
+		});
 	}
 });
 
 app.get('/stop', function(req, res){
   if (status) {
-		exec('./stop',
+		execFile('./stop',
 			function (error, stdout, stderr) {
 				console.log('stdout: ' + stdout);
 				console.log('stderr: ' + stderr);
