c375c56f4878e14a686ea2bbd865b44031a26db5
Carroll Yu <cyu@commonsense.org>
2016-07-26 14:21:52 -0700

    add restart route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var exec = require('child_process').exec;
 var execFile = require('child_process').execFile;
 var fs = require('fs');
 
@@ -13,7 +14,33 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res){
+function startServer(cb) {
+	fs.readFile(CONFIG_FILE, function(err, data) {
+    	var CONFIG = data ? JSON.parse(data) : [];
+    	var options = '';
+		for (setting in CONFIG) {
+			options += CONFIG[setting].key + ' ' + CONFIG[setting].value + ' ';
+		}
+		status = execFile('./start', [options], function (error, stdout, stderr) {
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+		});	
+		cb(null, 'starting server... ');
+	});	
+}
+
+function stopServer(cb) {
+	exec('./stop', function (error, stdout, stderr) {
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		}
+		status = null;
+		cb(null, 'stopping server... ');		
+	});
+}
+
+app.get('/', function(req, res) {
 	res.render('index');
 });
 
@@ -22,47 +49,41 @@ app.get('/start', function(req, res) {
   		res.send('server is already running!');
 	}
 	else {
-		fs.readFile(CONFIG_FILE, function(err, data) {
-	    	var CONFIG = data ? JSON.parse(data) : [];
-	    	var options = '';
-			for (setting in CONFIG) {
-				options += CONFIG[setting].key + ' ' + CONFIG[setting].value + ' ';
-			}
-			status = execFile('./start', [options], function (error, stdout, stderr) {
-					console.log('stdout: ' + stdout);
-					console.log('stderr: ' + stderr);
-
-				if (error !== null) {
-					console.log('exec error: ' + error);
-				}
-			});	
-			res.send('starting server...');
+		startServer(function(err, response) {
+			res.send(response);
 		});
 	}
 });
 
-app.get('/stop', function(req, res){
-  if (status) {
-		execFile('./stop',
-			function (error, stdout, stderr) {
-				console.log('stdout: ' + stdout);
-				console.log('stderr: ' + stderr);
-			if (error !== null) {
-				console.log('exec error: ' + error);
-			}
-		});
-		status = null;
-  	res.send('stopping server...');
-	}
-	else {
+app.get('/stop', function(req, res) {
+	if (!status) {
 		res.send('server has already stopped!');
 	}
+	else {
+		stopServer(function(err, response) {
+			res.send(response);
+		});		
+	}
 });
 
-app.get('/restart', function(req, res){
-  res.end('ok');
+app.get('/restart', function(req, res) {
+	if (status) {
+		stopServer(function(err, response) {
+			res.write(response);
+			startServer(function(err, response) {
+				res.write(response);
+				res.end();
+			});		
+		});		
+	}
+	else {
+		startServer(function(err, response) {
+			res.write(response);
+			res.end();
+		});
+	}
 });
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
 })
